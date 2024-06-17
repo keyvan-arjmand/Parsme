f1195f80acf55daeeae8de133dc2b6e9a19566fe
Key1 <keyvan.arjmnd@gmail.com>
2024-06-17 17:48:48 +0330

    Panel

---
+++ Domain/Migrations/20240617134501_color with inventory.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class colorwithinventory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Inventory",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUri",
+                table: "ProductColors");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Inventory",
+                table: "ProductColors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Inventory",
+                table: "ProductColors");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Inventory",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUri",
+                table: "ProductColors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
