c77ca1179ff543a4b95a66a57d0035dc8357c6b2
Key1 <keyvan.arjmnd@gmail.com>
2024-07-22 11:58:25 +0330

    Last

---
+++ Domain/Migrations/20240718114133_add Sheba Code meli.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class addShebaCodemeli : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "AspNetUsers",
+                newName: "Sheba");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NationalCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NationalCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Sheba",
+                table: "AspNetUsers",
+                newName: "Address");
+        }
+    }
+}
---
+++ Domain/Migrations/20240719073504_add banner.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class addbanner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BigBannerMiddle1Col",
+                table: "Banners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BigBannerMiddle1ColHref",
+                table: "Banners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BigBannerMiddle2Col",
+                table: "Banners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BigBannerMiddle2ColHref",
+                table: "Banners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BigBannerMiddle1Col",
+                table: "Banners");
+
+            migrationBuilder.DropColumn(
+                name: "BigBannerMiddle1ColHref",
+                table: "Banners");
+
+            migrationBuilder.DropColumn(
+                name: "BigBannerMiddle2Col",
+                table: "Banners");
+
+            migrationBuilder.DropColumn(
+                name: "BigBannerMiddle2ColHref",
+                table: "Banners");
+        }
+    }
+}
