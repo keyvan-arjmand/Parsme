6b92056e451bcfa5fb1554b1e10d8ea9eacb24ca
Key1 <keyvan.arjmnd@gmail.com>
2024-09-30 01:56:21 +0330

    mig

---
+++ Domain/Migrations/20240925234039_Desc sale service.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class Descsaleservice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DescReceivePerson",
+                table: "SaleServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DescShippingToday",
+                table: "SaleServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescReceivePerson",
+                table: "SaleServices");
+
+            migrationBuilder.DropColumn(
+                name: "DescShippingToday",
+                table: "SaleServices");
+        }
+    }
+}
---
+++ Domain/Migrations/20240928120927_add ishow banner.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class addishowbanner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowTopBanner",
+                table: "SeoPage",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowTopBanner",
+                table: "SeoPage");
+        }
+    }
+}
