da6e60d69a2636d903c2803c1a11693e0ed143c6
k1 <keyvan.arjmnd@gmail.com>
2025-02-09 16:37:39 +0330

    color gu

---
+++ Domain/Entity/Product/ColorGuarantee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Common;
+
+namespace Domain.Entity.Product;
+
+public class ColorGuarantee:BaseEntity
+{
+    public int ProductColorId { get; set; }
+    [ForeignKey(nameof(ProductColorId))] public ProductColor? ProductColor { get; set; }
+    public int GuaranteeId { get; set; }
+    [ForeignKey(nameof(GuaranteeId))] public Guarantee? Guarantee { get; set; }
+    public double Price { get; set; }
+    public int Inventory { get; set; }
+}
